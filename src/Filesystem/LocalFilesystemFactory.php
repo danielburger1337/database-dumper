f0bb988235d9033c1c25dffb8cd63d1dba6c7beb
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2023-09-11 04:49:03 +0200

    Support "%kernel.project_dir%" placeholder for local filesystem DSN

--- src/Filesystem/LocalFilesystemFactory.php
+++ src/Filesystem/LocalFilesystemFactory.php
@@ -6,9 +6,16 @@ use App\Util\Dsn;
 use League\Flysystem\Filesystem;
 use League\Flysystem\FilesystemOperator;
 use League\Flysystem\Local\LocalFilesystemAdapter;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class LocalFilesystemFactory implements FilesystemFactoryInterface
 {
+    public function __construct(
+        #[Autowire(param: 'kernel.project_dir')]
+        private readonly string $kernelProjectDir
+    ) {
+    }
+
     public function supports(Dsn $dsn): bool
     {
         return $dsn->getScheme() === 'local' && $dsn->getHost() === 'local';
@@ -20,6 +27,11 @@ class LocalFilesystemFactory implements FilesystemFactoryInterface
 
         if (null === $path) {
             throw new \InvalidArgumentException('A "path" is required to create a local filesystem.');
+        }
+
+        $path = \str_replace('%kernel.project_dir%', $this->kernelProjectDir, $path);
+        if (\str_starts_with($path, '//')) {
+            $path = \substr($path, 1);
         }
 
         return new Filesystem(new LocalFilesystemAdapter($path));
